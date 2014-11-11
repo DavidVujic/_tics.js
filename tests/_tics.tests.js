5af35b83a94da11e2a454a95c980c876b842769a
David Vujic <david@vujic.se>
2014-11-11 12:24:18 +0100

    Unit test refactorings

--- tests/_tics.tests.js
+++ tests/_tics.tests.js
@@ -1,70 +1,62 @@
-/*global test:true, module:true, strictEqual:true, ok:true, _tics: true, interceptor:true, calls:true */
+/*global test:true, module:true, strictEqual:true, ok:true, _tics: true, interceptor:true, calls:true, testHelper:true */
 
-var fake = null;
+(function () {
+	var fake;
+	
+	module('object initialization');
 
-module('object initialization');
+	test('is global object added', function () {
+		ok(_tics, 'ok');
+	});
 
-test('is global object added', function () {
-	ok(_tics, 'ok');
-});
+	test('is initialized', function () {
 
-test('is initialized', function () {
-
-	fake = {
-		trackPage: function () {},
-		track: function () {}
-	};
-
-	strictEqual(_tics.init(fake), true, 'strictEqual');
-});
-
-module('API tests', {
-	setup: function() {
-		fake = interceptor.add({
+		fake = {
 			trackPage: function () {},
 			track: function () {}
-		}, calls.add);
+		};
 
-		var input = window.document.createElement('input');
-		input.type = 'text';
-		input.className = 'element-added';
-		input.id = 'first-name-field';
+		strictEqual(_tics.init(fake), true, 'strictEqual');
+	});
 
-		window.document.getElementsByTagName('body')[0].appendChild(input);
-	}, teardown: function() {
-		fake = null;
-		var elementsAdded = window.document.querySelectorAll('.element-added');
-		var i;
+	module('API tests', {
+		setup: function() {
+			fake = interceptor.add({
+				trackPage: function () {},
+				track: function () {}
+			}, calls.add);
 
-		for(i = 0; i < elementsAdded.length; i += 1) {
-			elementsAdded[i].parentNode.removeChild(elementsAdded[i]);
+			testHelper.createInputField('first-name-field');
+		}, 
+		teardown: function () { 
+			fake = null;
+			testHelper.clearAdded();
 		}
+	});
 
-	}
-});
+	test('track page was called', function () {
+		_tics.init(fake);
+		_tics.page();
 
-test('track page was called', function () {
-	_tics.init(fake);
-	_tics.page();
+		strictEqual(fake.trackPage.calls, 1, 'strictEqual');
+	});
 
-	strictEqual(fake.trackPage.calls, 1, 'strictEqual');
-});
+	test('track events should not be called until change', function () {
+		_tics.init(fake);
+		_tics.events();
 
-test('track events should not be called until change', function () {
-	_tics.init(fake);
-	_tics.events();
-	
-	strictEqual(fake.track.calls, 0, 'strictEqual');
-});
+		strictEqual(fake.track.calls, 0, 'strictEqual');
+	});
 
-test('track events was called', function () {
-	_tics.init(fake);
-	_tics.events();
+	test('track events was called', function () {
+		_tics.init(fake);
+		_tics.events();
 
-	var elm = window.document.getElementById('first-name-field');
-	elm.value = 'hello world';
+		var elm = window.document.getElementById('first-name-field');
+		elm.value = 'hello world';
 
-	_tics.helper.trigger('change', elm);
-	
-	strictEqual(fake.track.calls, 1, 'strictEqual');
-});
+		_tics.helper.trigger('change', elm);
+
+		strictEqual(fake.track.calls, 1, 'strictEqual');
+	});
+}());
---
+++ tests/testHelper.js
@@ -0,0 +1,25 @@
+var testHelper = (function (){
+
+	var createInput = function (id) {
+		var input = window.document.createElement('input');
+			input.type = 'text';
+			input.className = 'element-added';
+			input.id = id;
+
+			window.document.getElementsByTagName('body')[0].appendChild(input);
+	};
+
+	var remove = function () {
+		var elementsAdded = window.document.querySelectorAll('.element-added');
+			var i;
+
+			for(i = 0; i < elementsAdded.length; i += 1) {
+				elementsAdded[i].parentNode.removeChild(elementsAdded[i]);
+			}
+	};
+
+	return {
+		createInputField: createInput,
+		clearAdded: remove
+	};
+}());
