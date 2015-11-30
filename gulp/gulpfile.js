3b89d8c607c160566c0df11461e780a14c4bc43e
David Vujic <david@vujic.se>
2015-11-30 20:30:15 +0100

    Added npm init. Using eslint. Updated gulpfile

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,19 +5,29 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var qunit = require('node-qunit-phantomjs');
 
-gulp.task('minify', function () {	
-	gulp.src(['../_tics.js','../_tics.helper.js', '../_tics.functions.js', '../_tics.gaProvider.js'])
+gulp.task('minify', function () {
+	var files = [
+		'../_tics.js', '../_tics.helper.js',
+		'../_tics.functions.js',
+		'../_tics.gaProvider.js'
+	];
+
+	gulp.src(files)
 		.pipe(concat('_tics.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('../'));
 });
 
-gulp.task('qunit', function() {
-    qunit('../tests/testrunner.html', { 'verbose': true });
+gulp.task('qunit', function () {
+	qunit('../tests/testrunner.html', {
+		'verbose': false
+	});
 });
 
-gulp.task('qunit-release', function() {
-    qunit('../tests/testrunner-release.html', { 'verbose': true });
+gulp.task('qunit-release', ['minify'], function () {
+	qunit('../tests/testrunner-release.html', {
+		'verbose': false
+	});
 });
 
 gulp.task('default', ['minify', 'qunit', 'qunit-release']);
